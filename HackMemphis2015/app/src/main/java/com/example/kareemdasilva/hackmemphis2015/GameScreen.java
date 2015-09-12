6f171ef42a13404383a7ff0c86dd5c76ae0080c6
Kareem Dasilva <kareemdasilva@Kareems-MacBook-Pro.local>
2015-09-12 14:34:00 -0500

    added 2 player

--- HackMemphis2015/app/src/main/java/com/example/kareemdasilva/hackmemphis2015/GameScreen.java
+++ HackMemphis2015/app/src/main/java/com/example/kareemdasilva/hackmemphis2015/GameScreen.java
@@ -1,9 +1,12 @@
 package com.example.kareemdasilva.hackmemphis2015;
 
+import android.graphics.Color;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -14,17 +17,48 @@ import java.util.TimerTask;
 
 
 public class GameScreen extends ActionBarActivity {
+    public class Player {
+        int lifePoints;
+        int answers;
+
+        public  Player(int playerLifepoints, int playerAnswers) {
+            lifePoints = playerLifepoints;
+            answers = playerAnswers;
+        }
+
+        public void lostLifePoints(int currentPoints){
+           lifePoints =   currentPoints;
+
+        }
+
+
+    }
+
     private Timer gameTimer;
     private int gameCounter = 10;
     private TextView counter;
+
+    //Players
+    public Player player1;
+    //public Player player2;
+
+    private Button player1Button;
+    private Button player2Button;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game_screen);
+
+        player1Button = (Button) findViewById(R.id.player1Button);
+        player2Button = (Button) findViewById(R.id.player2Button);
+        player1Button.setBackgroundColor(Color.GREEN);
+        player2Button.setBackgroundColor(Color.BLUE);
+
         counter = (TextView) findViewById(R.id.timer); // Text for GameTimer
         gameTimer = new Timer();
+        player1 = new Player(8000,0);
         gameTimer.scheduleAtFixedRate(new TimerTask() {
 
             @Override
@@ -34,9 +68,15 @@ public class GameScreen extends ActionBarActivity {
                     public void run() {
                         if (gameCounter == 0 ) {
                             gameTimer.cancel();
+
+
                         }
                         counter.setText(String.valueOf(gameCounter)); // you can set it to a textView to show it to the user to see the time passing while he is writing.
                         gameCounter--;
+
+
+
+
                         //checks if game counter hits zero
 
                     }
@@ -44,6 +84,12 @@ public class GameScreen extends ActionBarActivity {
 
             }
         }, 1000, 1000);
+    }
+    public  void player1Answer(View view){
+        gameTimer.cancel();
+    }
+    public void player2Answer(View view){
+        gameTimer.cancel();
     }
 
     @Override
@@ -67,4 +113,5 @@ public class GameScreen extends ActionBarActivity {
 
         return super.onOptionsItemSelected(item);
     }
+
 }
